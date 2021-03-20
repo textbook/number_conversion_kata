11f71a7351af38655d6f33a06ed492d91c8d7f92
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2021-03-20 08:57:45 +0000

    Cover all simple words [red]

--- index.test.js
+++ index.test.js
@@ -3,14 +3,32 @@ const { expect } = require("chai");
 const { convert } = require(".");
 
 describe("number conversion", () => {
-  [
-    [0, "zero"],
-    [1, "one"],
-    [2, "two"],
-    [3, "three"],
-  ].forEach(([input, output]) => {
-    it(`converts ${input} to '${output}'`, () => {
-      expect(convert(input)).to.equal(output);
+  describe("for numbers 0 to 19", () => {
+    [
+      [0, "zero"],
+      [1, "one"],
+      [2, "two"],
+      [3, "three"],
+      [4, "four"],
+      [5, "five"],
+      [6, "six"],
+      [7, "seven"],
+      [8, "eight"],
+      [9, "nine"],
+      [10, "ten"],
+      [11, "eleven"],
+      [12, "twelve"],
+      [13, "thirteen"],
+      [14, "fourteen"],
+      [15, "fifteen"],
+      [16, "sixteen"],
+      [17, "seventeen"],
+      [18, "eighteen"],
+      [19, "nineteen"],
+    ].forEach(([input, output]) => {
+      it(`converts ${input} to '${output}'`, () => {
+        expect(convert(input)).to.equal(output);
+      });
     });
   });
 });
