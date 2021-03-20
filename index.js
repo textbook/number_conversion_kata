8607a1f1c7728c6bf43d2a65bf93152d01da06be
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2021-03-20 08:59:33 +0000

    Implement all simple words [green]

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-const words = ["zero", "one", "two", "three"];
-
 function convert(number) {
   return words[number];
 }
+
+const words = [
+  "zero",
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+  "ten",
+  "eleven",
+  "twelve",
+  "thirteen",
+  "fourteen",
+  "fifteen",
+  "sixteen",
+  "seventeen",
+  "eighteen",
+  "nineteen",
+];
 
 module.exports.convert = convert;
