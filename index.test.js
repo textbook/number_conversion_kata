3f940a9c3ecfa825889f152e7cb0c38cbd0a916e
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2021-03-20 08:55:15 +0000

    Parameterise tests [refactor]

--- index.test.js
+++ index.test.js
@@ -3,19 +3,14 @@ const { expect } = require("chai");
 const { convert } = require(".");
 
 describe("number conversion", () => {
-  it("converts 0 to 'zero'", () => {
-    expect(convert(0)).to.equal("zero");
-  });
-
-  it("converts 1 to 'one'", () => {
-    expect(convert(1)).to.equal("one");
-  });
-
-  it("converts 2 to 'two'", () => {
-    expect(convert(2)).to.equal("two");
-  });
-
-  it("converts 3 to 'three'", () => {
-    expect(convert(3)).to.equal("three");
+  [
+    [0, "zero"],
+    [1, "one"],
+    [2, "two"],
+    [3, "three"],
+  ].forEach(([input, output]) => {
+    it(`converts ${input} to '${output}'`, () => {
+      expect(convert(input)).to.equal(output);
+    });
   });
 });
