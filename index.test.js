6c2669dec413b4d8e2a964a4a31298e705cad45e
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2021-03-20 08:47:23 +0000

    Test the one case [red]

--- index.test.js
+++ index.test.js
@@ -6,4 +6,8 @@ describe("number conversion", () => {
   it("converts 0 to 'zero'", () => {
     expect(convert(0)).to.equal("zero");
   });
+
+  it("converts 1 to 'one'", () => {
+    expect(convert(1)).to.equal("one");
+  });
 });
